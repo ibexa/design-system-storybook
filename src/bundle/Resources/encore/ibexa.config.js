dbca6081a19e1e13b32ffcfd0e4c2f2c089a08bd
Michał Grabowski <michal.grabowski@ibexa.co>
2025-07-18 11:16:21 +0200

    change init comps dir

--- src/bundle/Resources/encore/ibexa.config.js
+++ src/bundle/Resources/encore/ibexa.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = (Encore) => {
     Encore.addEntry('ibexa-design-system-storybook-js', [
-        path.resolve('./vendor/ibexa/design-system-twig/src/bundle/Resources/public/ts/init_components.ts')]
-    );
+        path.resolve('./public/bundles/ibexadesignsystemtwig/ts/init_components.ts'),
+    ]);
     Encore.addEntry('ibexa-design-system-storybook-css', [path.resolve(__dirname, '../public/scss/storybook.scss')]);
 };
