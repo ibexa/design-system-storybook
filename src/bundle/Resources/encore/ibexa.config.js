7b245b4d2e3ca005e651bf35485d9ba9059845aa
Michał Grabowski <michal.grabowski@ibexa.co>
2025-07-25 13:57:31 +0200

    IBX-10258: Input Text - clear action (#5)

--- src/bundle/Resources/encore/ibexa.config.js
+++ src/bundle/Resources/encore/ibexa.config.js
@@ -1,5 +1,8 @@
 const path = require('path');
 
 module.exports = (Encore) => {
+    Encore.addEntry('ibexa-design-system-storybook-js', [
+        path.resolve('./public/bundles/ibexadesignsystemtwig/ts/init_components.ts'),
+    ]);
     Encore.addEntry('ibexa-design-system-storybook-css', [path.resolve(__dirname, '../public/scss/storybook.scss')]);
 };
